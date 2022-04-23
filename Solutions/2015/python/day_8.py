b1a1a342f7d409e005e63ce2da50ec5efb91a9d8
Whitthenstein <joaombranco92@gmail.com>
2022-04-23 02:48:39 +0100

    changes to day 8

--- Solutions/2015/python/day_8.py
+++ Solutions/2015/python/day_8.py
@@ -1,43 +1,8 @@
 from setup_solutions import getPuzzleInput
-import numpy as np
 
 puzzleInput = getPuzzleInput("2015", "Day_8.txt")
 puzzle = puzzleInput
-
-#puzzle = [r'""', r'"abc"', r'"aaa\"aaa"', r'"\x27"']
-
-totalCharsOfStringCode = 0
-totalCharsInMem = 0
-
-# Part - One
-def countCharsInCode(string: str):
-    return len(string)
     
-
-def countCharsInMem(string: str):
-    return len(eval(string))
-
-def countChars(string: str):
-    global totalCharsOfStringCode
-    global totalCharsInMem
-
-    totalCharsOfStringCode += countCharsInCode(string)
-    totalCharsInMem += countCharsInMem(string)
-
-def countTotalChars():
-    for line in puzzle:
-        countChars(line.replace("\n", ""))
-
-def getTotalNumberCharacters1():
-    countTotalChars()
-    return totalCharsOfStringCode - totalCharsInMem
-
-# Part - Two
-def getTotalNumberCharacters2():
-    countTotalChars()
-    return totalCharsOfStringCode - totalCharsInMem
-    
-
 # Script runs here
 print("Advent of Code - 2015: Day Eight")
 print("Part-One Answer:", sum(len(s.replace("\n", "")) - len(eval(s.replace("\n", ""))) for s in puzzle))
