3c2ba65058692d6268e8bb9f47d1c0cc1be9c6f5
Whitthenstein <joaombranco92@gmail.com>
2022-12-04 18:45:16 +0000

    minor fix to python solutions setup

--- Solutions/python/SetupSolutions/SetupSolutions.py
+++ Solutions/python/SetupSolutions/SetupSolutions.py
@@ -1,7 +1,6 @@
 import os
 
 def getPuzzleInput(year: str, day: str):
-    print(os.getcwd())
     inputPath = os.path.join("..", "..", "..", "Inputs", year, day)
     scriptInDirectory = os.path.dirname(os.path.realpath(__file__))
     puzzlePath = os.path.join(scriptInDirectory, inputPath)
